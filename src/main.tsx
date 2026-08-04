eb3beca193ea9eefc7d7f4629b4cb01a5911879a
Figma Make <build@figma.com>
2026-08-04 19:24:06 +0000

    feat: add /price-card route via react-router

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ if (!globalThis.Buffer) (globalThis as any).Buffer = Buffer
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/price-card" element={<App />} />
+        <Route path="*" element={null} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
